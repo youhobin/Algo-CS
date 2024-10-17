5d68ec187a8a8850ee6f1ce47dc1542c9bdc633d
youhobin <ghdb132@naver.com>
2024-10-17 14:10:44 +0900

    9012 괄호

---
+++ BJ/silver/9012/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            String PS = br.readLine();
+
+            if (isValidVPS(PS)) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static boolean isValidVPS(String ps) {
+        String[] array = ps.split("");
+        Stack<Integer> stack = new Stack<>();
+
+        for (String a : array) {
+            if (a.equals("(")) {
+                stack.push(0);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
