2a70eb72098846858a42edaf96c092c068cbfbf4
youhobin <ghdb132@naver.com>
2024-10-18 17:32:08 +0900

    2164 카드2

---
+++ BJ/silver/2164/Main.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            int lastNum = queue.poll();
+            queue.offer(lastNum);
+        }
+
+        System.out.println(queue.poll());
+    }
+}
