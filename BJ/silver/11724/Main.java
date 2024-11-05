88f3bda759df5a7e197aa6cb6418c3265ddd5d08
youhobin <ghdb132@naver.com>
2024-11-05 17:58:33 +0900

    11724 연결 요소의 개수

---
+++ BJ/silver/11724/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+    static int[][] graph = new int[1001][1001];
+    static int V;
+    static int E;
+    static boolean[] visited = new boolean[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        V = sc.nextInt();
+        E = sc.nextInt();
+
+        int a,b;
+        for(int i = 0; i < E; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            graph[a][b] = graph[b][a] = 1;
+        }
+
+        int result = 0 ;
+
+        for(int i = 1; i <= V; i++) {
+            if(!visited[i]) {
+                dfs(i);
+                result++;
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+
+    public static void dfs(int index) {
+        visited[index] = true;
+
+        for(int i = 1; i <= V; i++) {
+            if(graph[index][i] == 1) {
+                dfs(i);
+            }
+        }
+    }
+}
